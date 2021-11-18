15652b98e040c7e5dbd0a47c604376215efc9292
Grzegorz Szczudlik <szczad+github@gmail.com>
2021-11-18 23:09:33 +0100

    Add Golang from archives

---
+++ .local/bootstrap/custom/bin/go.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# shellcheck disable=1091
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+source "${SCRIPT_DIR}/../lib/common.sh"
+
+version="1.17.3"
+
+while getopts ":v:" opt; do
+  case "$opt" in
+    v)
+      version="$OPTARG"
+      ;;
+    \?)
+      printf 'Must provide value for -%s\n' "$OPTARG"
+      ;;
+    :)
+      printf 'Invalid argument: -%s\n' "$OPTARG"
+      ;;
+  esac
+done
+
+
+os="$(uname -s)"
+case "$(uname -m)" in
+  i386)
+    arch="$(uname -m)"
+    ;;
+  x86_64)
+    arch=amd64
+    ;;
+  *)
+    printf 'Not supported arch!\n'
+    exit 1
+    ;;
+esac
+url="https://golang.org/dl/go${version}.${os,,}-${arch}.tar.gz"
+dest="${HOME}/.local/"
+
+printf 'Installing GO. OS: %s, Arch: %s, Version: %s\n' "${os,,}" "$arch" "$version"
+mkdir -p "$dest"
+if install_archive "$url" "$dest"; then
+  printf 'Installation successful! You can access Go at %s/go/bin\n' "$dest"
+else
+  printf 'Installation failed!\n'
+  return 1
+fi
---
+++ .local/bootstrap/custom/lib/common.sh
@@ -0,0 +1,21 @@
+install_archive() {
+  if [[ $# -lt 2 ]]; then
+    printf 'install_archive <url> <dest> [files...]\n'
+    return 1
+  fi
+
+  local url="$1" dest="$2" tmp code=0
+  shift 2
+  local files=("$@")
+  
+  tmp=$(mktemp) 
+  if curl -# -L -o "$tmp" "$url"; then
+    tar -xaf "$tmp" -C "$dest" "${files[@]}"
+  else
+    code=1
+  fi
+
+  rm "$tmp"
+  return $code
+}
+
